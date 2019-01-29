5816ce742838a1db2824269e031894d102468227
Jtmonkeyboy <43076688+Jtmonkeyboy@users.noreply.github.com>
2019-01-29 11:37:22 -0700

    Set up the code so it can run

---
+++ VectorProject/Controller/Controller.cpp
@@ -0,0 +1,19 @@
+//
+//  Controller.cpp
+//  VectorProject
+//
+//  Created by Runyan, Jacob on 1/28/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#include "Controller.hpp"
+
+Controller :: Controller()
+{
+    
+}
+
+void Controller :: start()
+{
+    
+};
---
+++ VectorProject/Controller/Controller.hpp
@@ -0,0 +1,23 @@
+//
+//  Controller.hpp
+//  VectorProject
+//
+//  Created by Runyan, Jacob on 1/28/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#ifndef Controller_hpp
+#define Controller_hpp
+
+#include <iostream>
+using namespace std;
+
+class Controller
+{
+private:
+public:
+    Controller();
+    void start();
+};
+
+#endif /* Controller_hpp */
